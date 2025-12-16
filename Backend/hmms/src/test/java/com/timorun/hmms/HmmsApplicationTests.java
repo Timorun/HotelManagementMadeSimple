ad9946c5aa3c9e87d24766d8809bed94eb1c2b65
s2509148 <t.p.runhaar@student.utwente.nl>
2025-12-16 19:31:40 +0100

    Setup Spring boot backend

---
+++ Backend/hmms/src/test/java/com/timorun/hmms/HmmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timorun.hmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
