e169e1639ed1df26ca008dfb5c0c41fbb756676b
s2509148 <t.p.runhaar@student.utwente.nl>
2026-05-14 23:06:27 +0200

    Improve analytics view

--- frontend/hmms/src/utils/excelExport.js
+++ frontend/hmms/src/utils/excelExport.js
@@ -6,3 +6,15 @@ export function exportRowsToExcel(rows, fileName, sheetName = 'Data') {
   XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
   XLSX.writeFile(workbook, fileName);
 }
+
+export function exportSheetsToExcel(sheets, fileName) {
+  const workbook = XLSX.utils.book_new();
+
+  Object.entries(sheets).forEach(([sheetName, rows]) => {
+    const safeRows = Array.isArray(rows) ? rows : [];
+    const worksheet = XLSX.utils.json_to_sheet(safeRows);
+    XLSX.utils.book_append_sheet(workbook, worksheet, sheetName.slice(0, 31));
+  });
+
+  XLSX.writeFile(workbook, fileName);
+}
